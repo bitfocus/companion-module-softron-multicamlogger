f25378357b6235f176158e6db676e4052683fb84
Peter Daniel <peterdaniel@peters-macbook-air.local>
2023-10-29 13:24:33 +0000

    1.0

--- variables.js
+++ variables.js
@@ -1,7 +1,14 @@
-module.exports = async function (self) {
-	self.setVariableDefinitions([
-		{ variableId: 'variable1', name: 'My first variable' },
-		{ variableId: 'variable2', name: 'My second variable' },
-		{ variableId: 'variable3', name: 'Another variable' },
-	])
+export function updateVariables() {
+	let variables = []
+
+	variables.push(
+		{ variableId: 'preview', name: 'Preview' },
+		{ variableId: 'previewLabel', name: 'Preview Label' },
+		{ variableId: 'document_name', name: 'Document Name' },
+		{ variableId: 'logging_state', name: 'Logging State' },
+		{ variableId: 'program', name: 'Program' },
+		{ variableId: 'programLabel', name: 'Program Label' },
+		{ variableId: 'timecode', name: 'Timecode' }
+	)
+	this.setVariableDefinitions(variables)
 }
